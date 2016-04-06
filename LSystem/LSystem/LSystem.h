15302fd3cc9a8bd9a2c8a4d552a99c76daef2882
Alfie Hanssen <alfiehanssen@gmail.com>
2016-04-06 12:55:53 -0400

    added ignore file

---
+++ LSystem/LSystem/LSystem.h
@@ -0,0 +1,19 @@
+//
+//  LSystem.h
+//  LSystem
+//
+//  Created by Hanssen, Alfie on 4/6/16.
+//  Copyright © 2016 Alfie Hanssen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LSystem.
+FOUNDATION_EXPORT double LSystemVersionNumber;
+
+//! Project version string for LSystem.
+FOUNDATION_EXPORT const unsigned char LSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LSystem/PublicHeader.h>
+
+
---
+++ LSystem/LSystem/LSystem.swift
@@ -0,0 +1,15 @@
+//
+//  LSystem.swift
+//  LSystem
+//
+//  Created by Hanssen, Alfie on 4/6/16.
+//  Copyright © 2016 Alfie Hanssen. All rights reserved.
+//
+
+import Foundation
+
+class Symbol
+{
+    
+}
+
